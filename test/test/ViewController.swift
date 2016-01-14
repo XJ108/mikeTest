ef30fa33f793b2ea99023a7a0f2cd4f38cee3897
XJ108 <123372341@qq.com>
2016-01-14 11:18:21 +0800

    修改数组元素

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         }
     }
 
-    let array111 : [Int] = [1, 2, 3, 4, 5]
+    let array111 : [Int] = [1, 2, 3, 4, 5, 9090990108]
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
