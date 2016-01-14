3ffa30ec34aab3e00d96ebbd486eb0284587d942
XJ108 <123372341@qq.com>
2016-01-14 11:37:00 +0800

    修改信息

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -26,24 +26,16 @@ class ViewController: UIViewController {
 
 
     let array111 : [Int] = [1, 2, 3, 4, 5]
->>>>>>> 68726463e7e95a92a9627d4ce38cff21c00b8c31
->>>>>>> 09531c63bf569ef5edd86c2587489aa4a2794c85
-    
-=======
-    let array111 : [Int] = [1, 2, 3, 4, 5, 9090990108]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    let array111 : [Int] = [1, 2, 3,4,5,6,7,8,9]
+    let array11112 : [Int] = [1, 2, 3, 4, 5, 9090990108]
+
+
+    let array11111 : [Int] = [1, 2, 3,4,5,6,7,8,9]
 
     let array1111 : [Int] = [1, 2, 3, 4, 5]
 
     let array346346546 : [Int] = [1, 2, 3, 4, 5]
     
-=======
-=======
->>>>>>> b0564237ca0cffb32c24d1d84a360508d437d94e
->>>>>>> d5a982a67a2338ab9ac8b5a832be9a68f5b48c18
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
