8158a073193722287874742bb55ca5a69fd809f0
XJ108 <123372341@qq.com>
2016-01-14 10:16:55 +0800

    新加了一个数组

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var array : [Int] = [1, 2, 3]
+        print(array)
     }
 
     override func didReceiveMemoryWarning() {
