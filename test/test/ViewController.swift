0413ee60a9f31ba99fffcfcf5e309ad7c76beb05
XJ108 <123372341@qq.com>
2016-01-14 11:44:46 +0800

    哦咯咯

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -24,16 +24,6 @@ class ViewController: UIViewController {
 
 
 
-
-    let array111 : [Int] = [1, 2, 3, 4, 5]
-
-    let array11112 : [Int] = [1, 2, 3, 4, 5, 9090990108]
-
-
-    let array11111 : [Int] = [1, 2, 3,4,5,6,7,8,9]
-
-    let array1111 : [Int] = [1, 2, 3, 4, 5]
-
     func fun1() -> Void {
         print("wwwww")
     }
